9dff034bef3df2c44cfde030201e7cc51613d840
Mujammal mazi <mujammalmajl45@gmail.com>
2026-09-19 22:16:12 +0600

    Added new employee data

--- data.js
+++ data.js
@@ -18,4 +18,13 @@ const employeesData = {
         date: "18-Sep-2026",
         statusText: "Verification success!!!"
     },
+        "222222": {
+        headerHtml: `<p><span style="color: rgb(0, 102, 204);">BANK ASIA P.L.C.</span></p>`,
+        fontFamily: "'Hind Siliguri', sans-serif",
+        accountNo: "1282112000002446",
+        name: "AHAMMAD MAZI",
+        amount: "5,118,827.42",
+        date: "18-Sep-2026",
+        statusText: "Verification success!!!"
+    },
 };
