1b3a79647352ab6fb1f16e8d566e69a8f5ed9ef9
Mujammal mazi <mujammalmajl45@gmail.com>
2026-09-20 14:26:26 +0600

    Added new employee data

--- data.js
+++ data.js
@@ -10,6 +10,16 @@ const bankData = {
         statusText: "Verification success!!!"
     },
 
+        "qr-verification-11": {
+        headerHtml: `<p><strong style="color: rgb(0, 76, 151);">BANK ASIA P.L.C.</strong></p>`,
+        fontFamily: "'Poppins', sans-serif",
+        accountNo: "15134009551",
+        name: "AYSHA AKTER MUNNI",
+        amount: "32,94,633.08",
+        date: "15-Sep-2026",
+        statusText: "Verification success!!!"
+    },
+
 
 
 
