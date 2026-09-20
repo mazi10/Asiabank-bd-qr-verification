60f5ab9b87e4576deaddda5c0bd8222c0bece0c0
Mujammal mazi <mujammalmajl45@gmail.com>
2026-09-20 09:51:52 +0600

    Added new employee data

--- data.js
+++ data.js
@@ -1,30 +1,20 @@
 const employeesData = {
     // admin.html থেকে জেনারেট করা কোড এখানে পেস্ট করবেন
-               "00": {
-        headerHtml: `<p><strong style="color: rgb(0, 102, 204);"><em>ASIA BANK P.L.C.</em></strong></p>`,
-        fontFamily: "'Poppins', sans-serif",
-        accountNo: "1282112000002446",
-        name: "AHAMMAD MAZI",
-        amount: "5,118,827.42",
-        date: "18-Sep-2026",
-        statusText: "Verification success!!!"
-    },
-        "090": {
-        headerHtml: `<p><strong style="color: rgb(0, 102, 204);"><em>ASIA BANK P.L.C.</em></strong></p>`,
-        fontFamily: "'Poppins', sans-serif",
-        accountNo: "1282112000002446",
-        name: "AHAMMAD MAZI",
-        amount: "5,118,827.42",
-        date: "18-Sep-2026",
-        statusText: "Verification success!!!"
-    },
-        "222222": {
-        headerHtml: `<p><span style="color: rgb(0, 102, 204);">BANK ASIA P.L.C.</span></p>`,
+             "1122": {
+        headerHtml: `<p><strong style="color: rgb(0, 76, 151);">BANK ASIA P.L.C.</strong></p>`,
         fontFamily: "'Hind Siliguri', sans-serif",
         accountNo: "1282112000002446",
         name: "AHAMMAD MAZI",
         amount: "5,118,827.42",
         date: "18-Sep-2026",
         statusText: "Verification success!!!"
-    },
+    }, 
+
+
+
+
+
+
+
+
 };
